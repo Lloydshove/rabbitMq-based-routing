b0b197e4982720090e88b95f2628d39ca19c03d4
lloydshove <email@lloydshove.com>
2023-06-05 10:45:55 +0800

    adding missing files

---
+++ latencyLogger/src/main/java/shovell/latencylogger/LatencyLoggerApplication.java
@@ -0,0 +1,13 @@
+package shovell.latencylogger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LatencyLoggerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LatencyLoggerApplication.class, args);
+    }
+
+}
---
+++ latencyLogger/src/test/java/shovell/latencylogger/LatencyLoggerApplicationTests.java
@@ -0,0 +1,13 @@
+package shovell.latencylogger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LatencyLoggerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
